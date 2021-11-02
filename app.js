47a62ae6e03172b7d0d5dd217cee35294e8fbc11
Ubaiduulah <ubaidullah5150@gmail.com>
2021-11-03 00:05:58 +0500

    Work Complete

---
+++ app.js
@@ -0,0 +1,297 @@
+          //OBJECTS
+          //Chap:1:ALERTS
+ //      ques 1:
+ //alert( "Good Morning")
+ //      ques 2:
+ //alert( "Error! Please enter a valid password")  
+//       ques 3:
+//alert( "Welcome to JS Land.... \n Happy coding!")     
+//       ques 4:
+//alert( "Welcome to JS Land....")
+//alert( "Happy Coding!")
+//       ques 5:
+//alert( "Hello.. I can run JS through my web browser's console")
+         //CHAP END//
+         //Chap : 2 :VARIABLE FOR STRINGS
+//        ques 1:
+//var firstValue =  "Username" ;
+//alert( firstValue)
+//        ques 2:
+//var FirstName = "Ubaidullah" ;
+//var lastName = "Nadeem" ;
+//var fullName = FirstName + lastName
+//alert( fullName )
+//      ques 3:
+//var firstValue =  "Hello World!" ;
+//alert( firstValue)
+//      ques 4:
+//var firstValue = "Jhone Doe" ;
+//alert( firstValue)
+//var firstValue = "15 years old" ;
+//alert( firstValue)
+//var firstValue = "Certified Mobile Application Development" ;
+//alert( firstValue)
+//      ques 5:
+//alert("Pizza \n Pizz \n Piz \n Pi \n P")
+//      ques 6:
+//var email = "My email address is ubaidullah5150@gmail.com" ;
+//alert( email )
+//      ques 7:
+//var book = "I am trying to learn from the Book A smarter \n way to learn javascript"
+//alert ( book )
+//      ques 8:
+//document.write( "Yah! I can write HTML content through Javascript")
+//      ques 9:
+//var value = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+//alert( value ) 
+              ///CHAP END///
+            ///Chap 3 : VARIABLE FOR STRINGS//
+//          ques 1:
+//var age = ( "I am 17 years old") ;
+//alert( age)
+//        ques 2:
+//var site = ( "You have visited this website 4 times ")
+//alert(site)
+//       ques 3:
+//var birthyear = prompt ( "Enter your Birth year") 
+//document.write("" + birthyear + "<br> " );
+//      ques 4:
+//var name = prompt ( "Enter Visitor’s name")
+//var quantity = prompt ( "Enter Quantity i.e. how many products a visitor wants to order")
+//var title = prompt ( "Enter Product title")
+//document.write("" + name + "" );
+//document.write("" + quantity + "" );
+//document.write("" + title + "" );
+          ///CHAP END///
+          ///Chap 4 : LEGAL AND ILLEGAL VARIABLE NAMES
+//       ques 1:
+/// 1: let posNo=0 , negNo=0 , zeroNo=0;
+/// 2: var posNo=0 , negNo=false, zeroNo='zero';
+/// 3: cont posNo=0 , negNo , zeroNo;
+//      ques 2:
+//            legal variables
+//1: $variable
+//2: _variable
+//3:  variable1
+//4:  &variable
+//5:  %variable
+//            illegal variables
+//1: @variable
+//2: 1variable
+//3: !variable
+//4: #variable
+//5: *variable 
+//      ques 3:(a)
+//document.write("Rules for naming JS variables")
+//      ques 3:(b)
+//Number, $ and _
+//       ques 3:(c)
+//$name, _name or name
+//       ques 3:(d)
+// case sensitive
+//       ques 3:(e)
+// keyword
+///       CHAP END
+///        chap 5: MATH EXPRESSIONS
+///       ques 1:
+///     Addition
+//var num1 = +prompt("Enter num1");
+//var num2 = +prompt("Enter num2");
+//var obtained = num1 + num2 ;
+//document.write("" + obtained + "<br> " );  
+//       ques 2:                     
+///       subtraction
+//var num1 = +prompt("Enter num1");
+//var num2 = +prompt("Enter num2");
+//var obtained = num1 - num2 ;
+//document.write("" + obtained + "<br> " );
+///       multiplication
+//var num1 = +prompt("Enter num1");
+//var num2 = +prompt("Enter num2");
+//var obtained = num1 * num2 ;
+//document.write("" + obtained + "<br> " );
+///       division
+//var num1 = +prompt("Enter num1");
+//var num2 = +prompt("Enter num2");
+//var obtained = num1 / num2 ;
+//document.write("" + obtained + "<br> " );
+///       modulous
+//var num1 = +prompt("Enter num1");
+//var num2 = +prompt("Enter num2");
+//var obtained = num1 % num2 ;
+//document.write("" + obtained + "<br> " );
+//     question 3:
+// samgh nhi araha ha
+//     ques 4 :
+//var num1 = +prompt("Enter num1");
+//var num2 = +prompt("Enter num2");
+//var obtained = num1 * num2 ;
+//document.write("" + obtained + "<br> " );
+//    ques 5:
+//document.write("Table of 4" + "<br>");
+//document.write("4*1 = 4" + "<br>");
+//document.write("4*2 = 8" + "<br>");
+//document.write("4*3 = 12" + "<br>");
+//document.write("4*4 = 16" + "<br>");
+//document.write("4*5 = 20" + "<br>");
+//document.write("4*6 = 24" + "<br>");
+//document.write("4*7 = 28" + "<br>");
+//document.write("4*8 = 32" + "<br>");
+//document.write("4*9 = 36" + "<br>");
+//document.write("4*10 = 40" + "<br>");
+//    ques 6:
+// samgh nhi araha ha
+//    ques 7:
+//var price1 = +prompt ( "Enter Price of item 1 ")
+//var price2 = +prompt ( "Enter Price of item 2")
+//var item1 = +prompt ( "Enter Ordered quantity of item 1")
+//var item2 = +prompt ( "Enter Ordered quantity of item 2")
+//var charges = +prompt ( "Enter Shopping Charges")
+//var total = price1 + price2 + item1 + item2 + charges ; 
+//document.write("Shopping Cart" + "<br>" ) ;
+//document.write("Price of item 1 is " + price1 + "<br>" );
+//document.write("Price of item 2 is " + price2 + "<br>" );
+//document.write("Ordered quantity of item 1 is " + item1 + "<br>" );
+//document.write("Ordered quantity of item 2 is " + item2 + "<br>" );
+//document.write("Shopping Charges " + charges + "<br><br>" );
+//document.write("Total coast of your order is " + total + "<br>" );
+//     ques 8 :
+//document.write("Marksheet" + "<br>" ) ;
+//var totalmarks = +prompt ( "Enter Total Marks")
+//var obtainedmarks = +prompt ( "Enter Obtained Marks")
+//var percentage = obtainedmarks/totalmarks*100 ;
+//document.write("Total Marks =  " + totalmarks + "<br>" );
+//document.write("Obtained Marks = " + obtainedmarks + "<br>" );
+//document.write("Percentage = " + percentage + "<br>" );
+//     ques 9 :
+//document.write("Currency in PKR" + "<br>" ) ;
+//document.write("Total Currency in PKR: 1748" + "<br>" ) ;
+//     ques 10 :
+//var num1 = 5 ;
+//var num1 = 10 ;
+//var num1 = 2 ;
+//var result = 5 * 10 / 2 ;
+//document.write("Answer:" + result + "<br>")
+//    ques 11 :
+//document.write("Age Calculator" + "<br>")
+//var year = +prompt ( "Enter Current year")
+//var age = +prompt ( "Enter birth year")
+//var result = year - age ;
+//document.write("Current year is " + year + "<br>" ) ;
+//document.write("Birth year is " + age + "<br>" ) ;
+//document.write("Age is "+ result + "<br>")
+//    ques 12:
+//samghna ha
+//    Chap END
+//     Chap 6-9 MATHS EXPRESSIONS
+///    ques 1
+//var a = +prompt ("Enter value")
+//var b = ++a ;
+//var c = a++ ;
+//var d = --a ;
+//var e = a-- ;
+//document.write("The value of a is : " + a + "<br><br>" ) ;
+//document.write("The value of ++a is :  "  + b + "<br>" ) ;
+//document.write("Now the value of a is :  " + b + "<br><br>" ) ;
+//document.write("The value of a++ is :  " + c + "<br>" ) ;
+//document.write("Now the value of a is :  " + c + "<br><br>" ) ;
+//document.write("The value of --a is :  " + c + "<br>" ) ;
+//document.write("Now the value of a is :  " + d + "<br><br>" ) ;
+//document.write("The value of a-- is : " + d + "<br>" ) ;
+//document.write("Now the value of a is :  " + e + "<br><br>" ) ;
+//    ques 2:
+//var a = +prompt ("Enter value a")
+//var b = +prompt ("Enter value b")
+//var a = --a - --b;
+//var b = --a - --b + ++b;
+//var result = --a - --b + ++b + b-- ;
+//document.write("a is " + a + "<br>" )
+//document.write("b is " + b + "<br>" )
+//document.write("Result is " + result + "<br>" )
+//     ques 3
+//var name = prompt ("Enter Your Name")
+//document.write("Welcome " + name + "<br>" )
+//     CHAP END
+//     chap 9-11: 
+//      ques 1:
+//var city = prompt ("Enter your city") ;
+//if(city == "karachi"){
+  //alert("Welcome to the city of lights")
+//}
+//else{
+ // alert("Welcome")
+//}
+//    ques 2:
+//var userGender = prompt ("Enter your Gender")
+//if( userGender == "male" ){
+//alert("Good Morning Sir!")
+//}
+//else if( userGender == "female" ){
+ // alert("Good Morning Maam!")
+ // }
+//else{
+ //alert("Please enter correct gender!")
+//}
+//      ques 3:
+//var light = prompt ("Enter Signal light")
+//if( light == "Red" ){
+//alert("Must stop")
+//}
+//else if( light == "Yellow" ){
+ // alert("Ready to move")
+ // }
+ // else if( light == "Green" ){
+ //   alert("Move now")
+ //   }
+//else{
+ //alert("Please enter correct light!")
+//}
+//     ques 4:
+//var fuel = prompt ("Enter your fuel in liters")
+//if( fuel == 0.25 ){
+//alert("Please refill your fuel")
+//}
+//else if( fuel > 0.25 ){
+//  alert("You have much fuel")
+//  }
+//else{
+//alert("please enter correct fuel!")
+// }
+///    ques 10:
+//var T = prompt ("Please Enter the Temperature")
+//if( T > 40 ){
+//alert("It is too hot outside")
+//}
+//else if( T > 30 ){
+ // alert("The Weather today is Normal")
+ // }
+ // else if( T > 20 ){
+ //   alert("Today’s Weather is cool")
+ //   }
+ //   else if( T > 10 ){
+ //     alert("OMG! Today’s weather is so Cool.")
+ //     }    
+//else{
+//alert("please enter correct Temperature!")
+ //}
+//     ques 11:
+//var num1 = +prompt("Enter number 1")
+//var opt = prompt("Enter opt")
+//var num2 = +prompt("Enter number 2")
+
+//if(opt === "+"){
+ // alert(num1 + num2)
+//}
+//else if(opt === "-"){
+ // alert(num1 - num2)
+//}
+//else if(opt === "*"){
+ // alert(num1 * num2)
+//}
+//else if(opt === "/"){
+ // alert(num1 / num2)
+//}
+//else{
+ // alert("invalid opt")
+//}
+//  Chap END
